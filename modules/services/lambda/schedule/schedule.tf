02c04a23c0a79256297d12d43c735ebad9df080c
Graham Pye <frahamp@gmail.com>
2020-10-22 22:24:42 +0100

    test using a map

--- modules/services/lambda/schedule/schedule.tf
+++ modules/services/lambda/schedule/schedule.tf
@@ -1,30 +1,28 @@
-variable "function_name" {
-  description = "The name of the function having a schedule set"
-  type        = string
-}
-variable "function_arn" {
-  description = "The arn of the function having a schedule set"
-  type        = string
-}
-variable "schedule_expression" {
-  description = "The expression for the schedule"
-  type        = string
+variable lambda_schedules {
+  description = "Map of the lambda schedules"
+  type = map
 }
 
 resource "aws_cloudwatch_event_rule" "event_rule" {
-  description         = "Schedule for ${var.function_name} with expression ${var.schedule_expression}"
-  schedule_expression = var.schedule_expression
+  for_each = var.lambda_schedules
+
+  description         = "Schedule for ${each.value.function_name} with expression ${each.value.schedule_expression}"
+  schedule_expression = each.value.schedule_expression
 }
 
 resource "aws_cloudwatch_event_target" "event_target" {
-  rule      = aws_cloudwatch_event_rule.event_rule.name
+  for_each = var.lambda_schedules
+
+  rule      = aws_cloudwatch_event_rule.event_rule[each.key].name
   target_id = "lambda"
-  arn       = var.function_arn
+  arn       = each.value.function_arn
 }
 
 resource "aws_lambda_permission" "lambda_permission" {
+  for_each = var.lambda_schedules
+
   action        = "lambda:InvokeFunction"
-  function_name = var.function_name
+  function_name = each.value.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.event_rule.arn
+  source_arn    = aws_cloudwatch_event_rule.event_rule[each.key].arn
 }
